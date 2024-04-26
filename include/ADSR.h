94118d4eae63d33e2fdda3231c2d4593c16d0b1d
jcxsun <150746946+jcxsun@users.noreply.github.com>
2024-04-26 15:37:02 -0500

    ADSR.h

--- include/ADSR.h
+++ include/ADSR.h
@@ -1,25 +1,3 @@
-//
-//  ADRS.h
-//
-//  Created by Nigel Redmon on 12/18/12.
-//  EarLevel Engineering: earlevel.com
-//  Copyright 2012 Nigel Redmon
-//
-//  For a complete explanation of the ADSR envelope generator and code,
-//  read the series of articles by the author, starting here:
-//  http://www.earlevel.com/main/2013/06/01/envelope-generators/
-//
-//  License:
-//
-//  This source code is provided as is, without warranty.
-//  You may copy and distribute verbatim copies of this document.
-//  You may modify and use this source code to create binary code for your own purposes, free or commercial.
-//
-//  1.01  2016-01-02  njr   added calcCoef to SetTargetRatio functions that were in the ADSR widget but missing in this code
-//  1.02  2017-01-04  njr   in calcCoef, checked for rate 0, to support non-IEEE compliant compilers
-//  1.03  2020-04-08  njr   changed float to double; large target ratio and rate resulted in exp returning 1 in calcCoef
-//
-
 #ifndef ADRS_h
 #define ADRS_h
 
